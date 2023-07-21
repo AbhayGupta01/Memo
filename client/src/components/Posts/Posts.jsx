1f06ab732e2a64f37ab78e5819870d7c40d2aa42
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-21 21:46:18 +0530

    no posts check

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -10,11 +10,11 @@ const Posts = () => {
     const {posts, isLoading} = useSelector((state) => state.posts)
     const classes = useStyles()
 
-    if (!{posts}.length && !isLoading) {
-        return (
-            <Typography align="center" variant="h4" component="h2">No Posts</Typography>
-        )
-    }
+    // if (!{posts}.length && !isLoading) {
+    //     return (
+    //         <Typography align="center" variant="h4" component="h2">No Posts</Typography>
+    //     )
+    // }
 
     if (isLoading) {
         return (
