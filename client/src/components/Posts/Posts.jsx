c1380455f111bd0d3f3213c8c1acb629647ac69a
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-21 21:13:09 +0530

    setCurrentId

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import {Grid, CircularProgress, Typography} from "@material-ui/core";
 import {useSelector} from "react-redux";
-
+import {setCurrentId} from "../../features/posts/postsSlice";
 import Post from "./Post/Post"
 import useStyles from "./styles"
 
-const Posts = () => {
+const Posts = (setCurrentId) => {
     const {posts, isLoading} = useSelector((state) => state.posts)
     const classes = useStyles()
 
@@ -15,24 +15,35 @@ const Posts = () => {
             <Typography align="center" variant="h4" component="h2">No Posts</Typography>
         )
     }
-
-    if (isLoading) {
-        return (
-            <div align="center">
-                <CircularProgress size="4em"/>
-            </div>
-        )
-    }
-
     return (
-        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-            {posts.map((post) => (
-                <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
-                    <Post post={post}/>
-                </Grid>
+        isLoading ? <CircularProgress /> : ( // posts?.length : post가 없어도 에러 내지 않도록
+          <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+            {posts?.map((post) => (
+              <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+                <Post post={post} setCurrentId={setCurrentId} />
+              </Grid>
             ))}
-        </Grid>
+          </Grid>
     )
+    );
+
+    // if (isLoading) {
+    //     return (
+    //         <div align="center">
+    //             <CircularProgress size="4em"/>
+    //         </div>
+    //     )
+    // }
+
+    // return (
+    //     <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+    //         {posts.map((post) => (
+    //             <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+    //                 <Post post={post}/>
+    //             </Grid>
+    //         ))}
+    //     </Grid>
+    // )
 }
 
 export default Posts;
