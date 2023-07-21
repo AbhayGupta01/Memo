52aace5b17c8bc580fdf28edfe25eeb6bb69f1a5
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-21 21:19:27 +0530

    server change

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -21,6 +21,6 @@ const postSchema = mongoose.Schema({
     }
 })
 
-const PostMessage = mongoose.model('PostMassage', postSchema)
+const PostMessage = mongoose.model('PostMessage', postSchema)
 
 export default PostMessage
