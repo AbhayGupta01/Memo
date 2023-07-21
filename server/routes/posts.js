d54398958b64706b541b546a9a7717722f7e3883
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-21 20:28:14 +0530

    deploy

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,18 +1,18 @@
-import {Router} from "express";
+import express from 'express';
 import {getPost, getPosts, getPostsBySearch, getCountOfPages, createPost, updatePost, deletePost, likePost, commentPost} from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
-const router = Router()
+const router = express.Router()
 
 router.get('/', getPosts)
-router.post('/', auth, createPost)
 router.get('/search', getPostsBySearch)
 router.get('/pagesCount', getCountOfPages)
 router.get('/:id', getPost)
 
 router.patch('/:id', auth, updatePost)
-router.delete('/:id', auth, deletePost)
 router.patch('/:id/like', auth, likePost)
+router.post('/', auth, createPost)
 router.post('/:id/comment', auth, commentPost)
+router.delete('/:id', auth, deletePost)
 
 export default router
